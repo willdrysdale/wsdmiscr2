0e1605b9abac83a1a72f1b7cd561da0fa1861392
willdrysdale <wsd500@york.ac.uk>
2024-03-21 14:15:58 +0000

    Add decimal_day_from_date
    previously known as get_DOY in wsdmiscr.
    No longer accepts data.frames
    The reverse of date_from_decimal_day

---
+++ R/decimal_date_from_day.R
@@ -0,0 +1,23 @@
+#' Decimal day from date
+#'
+#' convert a POSIXct in to a number representing day.decimalday, where Jan 1st 12:00 == 1.5
+#'
+#' @param date a POSIXct date
+#'
+#' @author W. S. Drysdale
+#'
+#' @export
+
+decimal_day_from_date = function(date){
+
+  DOY = as.numeric(lubridate::yday(date))
+  h = as.numeric(lubridate::hour(date) * 3600)
+  m = as.numeric(lubridate::minute(date) * 60)
+  s = as.numeric(lubridate::second(date))
+  all_seconds = h + m + s
+  decimal_day = all_seconds/86400
+  DOY_dd = DOY + decimal_day
+
+  DOY_dd
+
+}
